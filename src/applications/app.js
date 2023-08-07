02e14a55fe4f0557c8069fd7443a5cc9b709d461
李佳成 <lijiacheng0613@qs-tech.com>
2023-08-07 15:36:49 +0800

    [feature]: 要进行加载与挂载，需要通过应用状态区分应用库中哪些需要加载与挂载

--- src/applications/app.js
+++ src/applications/app.js
@@ -1,5 +1,14 @@
 import { reroute } from "../navigations/reroute";
-import { NOT_LOADED } from "./app.helper";
+import {
+  BOOTSTRAPPING,
+  LOADING_SOURCE_CODE,
+  MOUNTED,
+  NOT_BOOTSTRAPPED,
+  NOT_LOADED,
+  NOT_MOUNTED,
+  SKIP_BECAUSE_BROKEN,
+  shouldBeActive,
+} from "./app.helper";
 
 const apps = []; // 用来存放所有应用
 
@@ -21,4 +30,40 @@ export function registerApplication(appName, loadApp, activeWhen, customProps) {
   });
 
   reroute(); // 加载应用
+}
+
+/**
+ * 根据应用的状态来进行筛选，判断哪些应用是加载还是装载的
+ */
+export function getAppChanges() {
+  const appsToUnmount = []; // 要卸载的应用
+  const appsToLoad = []; // 要加载的应用
+  const appsToMount = []; // 要挂载的应用
+
+  apps.forEach((app) => {
+    // 根据当前hash判断是否被激活
+    const appShouldBeActive =
+      app.status !== SKIP_BECAUSE_BROKEN && shouldBeActive(app);
+    switch (app.status) {
+      case NOT_LOADED:
+      case LOADING_SOURCE_CODE:
+        if (appShouldBeActive) {
+          appsToLoad.push(app);
+        }
+        break;
+      case NOT_BOOTSTRAPPED:
+      case BOOTSTRAPPING:
+      case NOT_MOUNTED:
+        if (appShouldBeActive) {
+          appsToMount.push(app);
+        }
+        break;
+      case MOUNTED:
+        if (!appShouldBeActive) {
+          appsToUnmount.push(app);
+        }
+    }
+  });
+
+  return { appsToLoad, appsToUnmount, appsToMount };
 }
--- src/navigations/reroute.js
+++ src/navigations/reroute.js
@@ -1,6 +1,11 @@
+import { getAppChanges } from "../applications/app";
 import { started } from "../start";
 
 export function reroute() {
+  const { appsToLoad, appsToMount, appsToUnmount } = getAppChanges();
+
+  console.log(appsToLoad, appsToMount, appsToUnmount);
+
   if (started) {
     // start: 进行应用挂载
     return preformAppChanges(); // 根据路径来装载应用
