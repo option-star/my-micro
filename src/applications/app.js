129060994781532f37f4610167ec7c1c8b6809b4
李佳成 <lijiacheng0613@qs-tech.com>
2023-08-07 11:19:58 +0800

    [feature]: 存储注册时的应用

--- src/applications/app.js
+++ src/applications/app.js
@@ -1,1 +1,18 @@
-export function registerApplication() {}
+const apps = []; // 用来存放所有应用
+
+/**
+ * 注册应用
+ * @param {*} appName 应用名称
+ * @param {*} loadApp 加载的应用
+ * @param {*} activeWhen 当激活时会调用loadApp
+ * @param {*} customProps 自定义属性（用于全局通信）
+ */
+export function registerApplication(appName, loadApp, activeWhen, customProps) {
+  apps.push({
+    // 存储应用
+    name: appName,
+    loadApp,
+    activeWhen,
+    customProps,
+  });
+}
