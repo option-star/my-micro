2656d9b77dffd1ac006ba92e485dc2ca57fef233
李佳成 <lijiacheng0613@qs-tech.com>
2023-08-07 16:11:37 +0800

    [feature]: 通过将加载的生命周期函数暴露到应用上，完成应用加载

---
+++ src/lifecycles/load.js
@@ -0,0 +1,36 @@
+import {
+  LOADING_SOURCE_CODE,
+  NOT_BOOTSTRAPPED,
+} from "../applications/app.helper";
+
+function flattenFnArray(fns) {
+  // 兼容不是数组形式
+  fns = Array.isArray(fns) ? fns : [fns];
+
+  // props为组合前函数传参
+  // 通过promise链来链式调用Promise.resolve().then(() => fn1(props)).then(() => fn2(props))
+  return (props) =>
+    fns.reduce((p, fn) => p.then(() => fn(props)), Promise.resolve());
+}
+
+/**
+ * 将bootstrap、mount、unmount方法挂载在应用上
+ * @param {*} app 应用
+ */
+export async function toLoadPromise(app) {
+  // 开始加载，表示状态为加载中
+  app.status = LOADING_SOURCE_CODE;
+
+  // 获取对应的生命周期函数
+  let { bootstrap, mount, unmount } = await app.loadApp(app.customProps);
+
+  // 加载完毕，表示状态为启动前
+  app.status = NOT_BOOTSTRAPPED;
+
+  // 将对应的生命周期方法挂载在应用上
+  app.bootstrap = flattenFnArray(bootstrap);
+  app.mount = flattenFnArray(mount);
+  app.unmount = flattenFnArray(unmount);
+
+  return app;
+}
--- src/navigations/reroute.js
+++ src/navigations/reroute.js
@@ -1,4 +1,5 @@
 import { getAppChanges } from "../applications/app";
+import { toLoadPromise } from "../lifecycles/load";
 import { started } from "../start";
 
 export function reroute() {
@@ -13,14 +14,19 @@ export function reroute() {
     // register: 进行应用加载
     return loadApps(); // 预先加载应用
   }
+
+  /**
+   * 加载应用
+   */
+  async function loadApps() {
+    // 将需要加载的应用，取得其bootstrap、mount、unmount方法放到应用上，完成应用加载
+    let apps = await Promise.all(appsToLoad.map(toLoadPromise));
+
+    console.log(apps);
+  }
+
+  /**
+   * 根据路径来装载应用
+   */
+  async function preformAppChanges() {}
 }
-
-/**
- * 加载应用
- */
-async function loadApps() {}
-
-/**
- * 根据路径来装载应用
- */
-async function preformAppChanges() {}
